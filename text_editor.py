88518825dd0b55d8439afb6b2dc0975d144336be
valkon29 <konovalov.vr@phystech.edu>
2022-04-02 03:57:04 +0300

    Now it can do open/save/save as

--- text_editor.py
+++ text_editor.py
@@ -1,6 +1,56 @@
 import tkinter as tk
+from tkinter import filedialog
+
 
 class TextEditor:
-	def __init__(self,root):
-		pass
+    def __init__(self, window, *args):
+        self.window = window
+        self.current_file = None
+        self.file_types = (('text files', '*.txt'), ("Python Files", "*.py"), ('All files', '*.*'))
+        self.window.geometry("1000x800")
+        self.window.title("TextEditor")
+        self.text = tk.Text(self.window, bg="#333333", fg="#ffa500", font=("Helvetica", "16"), state="normal")
+        self.text.pack(expand=True)
+        self.status = tk.StringVar()
+        self.statusbar = tk.Label(window, textvariable=self.status, bd=1, relief=tk.SUNKEN, anchor=tk.N)
+        self.status.set('Ready for work!')
+        self.statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.menubar = tk.Menu(window)
+        self.set_menu(window)
+        self.set_hot_keys()
 
+    def set_menu(self, window):
+        window.config(menu=self.menubar)
+        file_menu = tk.Menu(self.menubar)
+        file_menu.add_command(label='Open', command=self.open_file, accelerator="Ctrl+O")
+        file_menu.add_command(label='Save', command=self.save, accelerator="Ctrl+S")
+        file_menu.add_command(label='Save as', command=self.save_as_file, accelerator="Ctrl+Q")
+        self.menubar.add_cascade(label="File", menu=file_menu)
+
+    def set_hot_keys(self):
+        self.window.bind('<Control-s>', self.save)
+        self.window.bind('<Control-o>', self.open_file)
+        self.window.bind('<Control-a>', self.save_as_file)
+
+    def open_file(self, *args):
+        self.status.set('Opening a file...')
+        self.text.delete("1.0", tk.END)
+        self.current_file = filedialog.askopenfilename(title="Open file", filetypes=self.file_types)
+        print(self.current_file)
+        file_to_read = open(self.current_file, 'r')
+        stuff = file_to_read.read()
+        self.text.insert(tk.END, stuff)
+        file_to_read.close()
+
+    def save(self, *args):
+        if self.current_file is not None:
+            input_file = open(self.current_file, 'w')
+            input_file.truncate(0)
+            input_file.write(self.text.get(1.0, tk.END))
+            input_file.close()
+
+    def save_as_file(self, *args):
+        self.current_file = filedialog.asksaveasfilename(defaultextension=".*", filetypes=self.file_types)
+        text_file = open(self.current_file, 'w')
+        text_file.write(self.text.get(1.0, tk.END))
+        text_file.close()
